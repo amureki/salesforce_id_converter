96b705547713185e176d7256f2cc48ee9a353f56
Rustem Sayargaliev <r.sayargaliev@gmail.com>
2018-07-06 15:32:14 +0200

    MVP with an algorithm

---
+++ main.py
@@ -0,0 +1,35 @@
+import string
+
+
+def get_bit(char) -> str:
+    return '1' if str(char).isupper() else '0'
+
+
+class Converter:
+    """
+    Convert 15 character Salesforce id into a 18 character id.
+
+    Algorithm is explained here:
+    http://salesforce.stackexchange.com/questions/1653/what-are-salesforce-ids-composed-of
+    """
+
+    def __init__(self, short):
+        self.short = short
+
+    def convert(self):
+        suffix = ''
+        chunks = [self.short[i:i+5] for i in range(0, len(self.short), 5)]
+        array = string.ascii_uppercase + string.digits[:6]
+
+        for chunk in chunks:
+            bits = str(int(''.join([get_bit(char) for char in chunk]), 2))
+            suffix += array[int(bits)]
+
+        return self.short + suffix
+
+
+if __name__ == '__main__':
+    short = '00558000001N0Ke'
+    long = '00558000001N0KeAAK'
+    converter = Converter(short=short)
+    assert converter.convert() == long

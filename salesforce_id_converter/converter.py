730e727f17be1681f2ff098c239a81df1534ad4b
Rustem Saiargaliev <amureki@users.noreply.github.com>
2018-07-16 18:17:22 +0200

    Fix algorithm (was missing reversion), update tests (#1)

--- salesforce_id_converter/converter.py
+++ salesforce_id_converter/converter.py
@@ -16,13 +16,19 @@ class Converter:
     def __init__(self, short):
         self.short = short
 
+    def _convert_chunk_to_bit(self, chunk):
+        binary_repr = ''.join(reversed([get_bit(char) for char in chunk]))
+        return str(int(binary_repr, 2))
+
     def convert(self):
         suffix = ''
+        # split id into three chunks
         chunks = [self.short[i:i+5] for i in range(0, len(self.short), 5)]
+        # construct an array of A-Z and 0-5
         array = string.ascii_uppercase + string.digits[:6]
 
         for chunk in chunks:
-            bits = str(int(''.join([get_bit(char) for char in chunk]), 2))
-            suffix += array[int(bits)]
+            bit_char = self._convert_chunk_to_bit(chunk)
+            suffix += array[int(bit_char)]
 
         return self.short + suffix
--- tests/test_converter.py
+++ tests/test_converter.py
@@ -2,9 +2,6 @@ from unittest import TestCase
 
 from salesforce_id_converter.converter import Converter, get_bit
 
-SALESFORCE_SHORT_ID = '00558000001N0Ke'
-SALESFORCE_LONG_ID = '00558000001N0KeAAK'
-
 
 class TestConverter(TestCase):
     def test_get_bit(self):
@@ -21,5 +18,12 @@ class TestConverter(TestCase):
             assert get_bit(char) == mapped
 
     def test_convert(self):
-        converter = Converter(short=SALESFORCE_SHORT_ID)
-        assert converter.convert() == SALESFORCE_LONG_ID
+        salesforce_test_ids = (
+            ('001A0000006Vm9u', '001A0000006Vm9uIAC'),
+            ('00558000001N0Ke', '00558000001N0KeAAK'),
+            ('00D0Q0000008aI9', '00D0Q0000008aI9UAI'),
+            ('0016E00000YbJrD', '0016E00000YbJrDQAV'),
+        )
+        for short_id, long_id in salesforce_test_ids:
+            converter = Converter(short=short_id)
+            assert converter.convert() == long_id
